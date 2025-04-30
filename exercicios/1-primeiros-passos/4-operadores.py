724c6cd2da8f5ff717d0ef4931e3f79578c7238c
WilliamMoraisPhilips <WilliamMorais.Weidgenant@philips.com>
2025-04-30 18:53:38 +0000

    Feitos

--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -2,9 +2,13 @@ ano_nascimento = 1989
 ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
+print(f"A idade de Gerlaine em sua formatura era {ano_formatura - ano_nascimento} anos de idade")
 
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
-
+print(ano_nascimento == ano_formatura)
+print(ano_nascimento > ano_formatura)
+print(ano_nascimento <= ano_formatura)
 
 # Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+print(ano_nascimento != ano_formatura & ano_formatura > ano_nascimento)
+print(ano_nascimento == ano_formatura | ano_formatura >= ano_nascimento)
--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,17 @@
 # Crie uma lista apenas com elementos numéricos
-
+numericos = [1,2,3,4,5,6]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+lista = [1,2.3,'4',True]
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+print(numericos)
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+print(numericos[0:-1:2])
 # Remova da lista o último item
-
+lista.pop()
+print(lista)
 # Insira na lista um novo item
-
+lista.append(False)
+print(lista)
 # Remova da lista um item específico
+lista.remove(False)
+print(lista)
